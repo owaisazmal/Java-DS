f3df2f052d3e0609930a87a0f53b4cea1269432b
Owais <owais.khan.244@my.csun.edu>
2023-11-25 15:21:50 -0800

    All the functions implemented

--- DS2Array.java
+++ DS2Array.java
@@ -1,9 +1,48 @@
 public class DS2Array {
 
-    public void insert(int i){
+    private int[] items;
+    private int count;
+
+    public DS2Array(int len){
+        items = new int[len];
+    }
+
+    public void insert(int item){
+        if (items.length == count){
+
+            int[] newItems = new int[count * 2];
+
+            for(int i = 0; i < count; i++){
+                newItems[i] = items[i];
+                items = newItems;
+             }
+        }
+        items[count++] = item;
+    }
+    public void removeAt(int index){
+        if (index < 0 || index >= count){
+            throw new IllegalArgumentException();
+        }
+    for (int i = index; i<count; i++){
+        items[i] = items[i + 1];
+    }
+
+    count--;
 
     }
+    // public int indexOf(int item){
+    //     for(int i = 0; i < count; i++){
+    //       if(items[i] == item){
+    //         return i;
+
+    //     return -1;
+    //       }  
+    //     }
+    // }
     public void print(){
-        System.out.println();
+        for(int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
     }
+
 }
--- DS2main.java
+++ DS2main.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 public class DS2main {
     public static void main(String[] args) {
-        int[] numbers = {2, 25, 265};
-        //int[] numbers = new int[3];
-        // numbers[0] = 5;
-        // numbers[1] = 25;
-        // numbers[2] = 265;
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
+        // int[] numbers = {2, 25, 265};
+        // //int[] numbers = new int[3];
+        // // numbers[0] = 5;
+        // // numbers[1] = 25;
+        // // numbers[2] = 265;
+        // System.out.println(numbers.length);
+        // System.out.println(Arrays.toString(numbers));
 
         //practice1
-        DS2Array num = new DS2Array(3);
+        DS2Array num = new DS2Array(5);
         num.insert(10);
-        num.insert(10);
-        num.insert(10);
+        num.insert(20);
+        num.insert(30);
+        num.insert(40);
+
+        num.removeAt(0);
         num.print();
 
     }
