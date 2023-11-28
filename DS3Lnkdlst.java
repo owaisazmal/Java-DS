dec4e3413e077626f8fe6dea170bafb75522566a
Owais <owais.khan.244@my.csun.edu>
2023-11-27 21:35:59 -0800

    linked list revised

---
+++ DS3Lnkdlst.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DS3Lnkdlst {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(14);
+        list.add(12);
+        //list.remove(0);
+        list.indexOf(10);
+        list.contains(10);
+        list.size();
+        list.toArray();
+        //System.out.println(list.indexOf(10));
+    }
+}
