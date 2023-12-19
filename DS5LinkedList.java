b4e644dc431c73a06d828273e6b01a04e17f6f37
Owais <owais.khan.244@my.csun.edu>
2023-12-19 14:43:13 -0800

    Linked list

---
+++ DS5LinkedList.java
@@ -0,0 +1,55 @@
+//Building Linked list form scratch
+
+public class DS5LinkedList {
+    private class Node {
+        public int value;
+        public Node next;
+        
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    
+    private Node first;
+    private Node last;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        // Item not found in the linked list
+        return -1;
+    }    
+}
