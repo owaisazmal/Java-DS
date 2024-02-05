c0fb90b54b2cd4b8358ae44ee94d91c5ba2a0714
Owais Khan <owaiskhan461@gmail.com>
2024-02-04 22:04:40 -0800

    Basic patern

--- paterns.java
+++ paterns.java
@@ -1,9 +1,10 @@
 public class paterns {
     public static void main(String[] args) {
 
-        int n,m;
+        int n,m, number;
         n = 4;
         m = 5;
+        number = 1;
 
     // Rectangle
     //     for (int i = 1; i <= n; i++){
@@ -17,13 +18,73 @@ public class paterns {
         // for (int i = 1; i <= n; i++){
         //     for (int j = 1; j <= m; j++){
         //         if(i == 1 || j == 1 || i == n || j == m){
-        //         System.err.print("*");
+        //         System.out.print("*");
         //     }else{
         //         System.out.print(" ");
         //     }
         //     }
         //     System.out.println();
         // }
+    //Half Pyramid
+            // for(int i = 1; i <= n; i++){
+            //     for(int j = 1; j <= i; j++){
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+    //Half Pyramid (Opposite)
+        // for(int i = n; i >= 1; i--){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
+    //Half Pyramid (180deg)
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= n - i; j++){
+        //         System.out.print(" ");
+        //     }
+        //         //inner loop -> star point
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //         }
+        //     System.out.println();
+        // }
+
+    //Print the Patern
+            // for(int i = 1; i <= n; i++){
+            //     for(int j = 1; j <= i; j++){
+            //         System.out.print(j + " ");
+            //     }
+            //     System.out.println();
+            // }
+    //Print the Patern inverted
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= (n-i+1); j++){
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+    //Floyd's Triangle
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <=i; j++){
+        //         System.out.print(number + " ");
+        //         number++;
+        //     }
+        //     System.out.println();
+        // }
+
+    //01 triangle
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if((i+j)%2 == 0)
+                System.out.print("1 ");
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
