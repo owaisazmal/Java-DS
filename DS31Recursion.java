c0e292d8d3a6ea82d06135df70150e285612e539
owaisazmal <owaiskhan461@gmail.com>
2024-08-28 22:11:41 -0700

    Hashtable

--- DS31Recursion.java
+++ DS31Recursion.java
@@ -2,6 +2,7 @@ public class DS31Recursion {
     public static void main(String[] args) {    
     walk(5);
     System.out.println(factorial(7));
+    System.out.println(power(2, 4));
 }
 //simple one to remember
     public static void walk(int steps){
@@ -13,6 +14,11 @@ public class DS31Recursion {
     public static int factorial(int num){
         if(num < 1) return 1; //base case
         return num * factorial(num-1);
+    }
+
+    public static int power(int num, int pow){
+        if(pow < 1) return 1; //base case
+        return num * power(num, pow-1);
     }
 }
 
---
+++ DS32HashTables.java
@@ -0,0 +1,19 @@
+import java.util.Hashtable;
+
+public class DS32HashTables {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> table = new Hashtable<>(10);
+
+        table.put(123, "devaansh");
+        table.put(614, "Omar");
+        table.put(456, "Azrin");
+        table.put(365, "Nawshin");
+        table.put(100, "Owais");
+
+        table.remove(365);
+
+        for(Integer key: table.keySet()){
+            System.out.println(key.hashCode()%10 + "\t" + key+ "\t" + table.get(key));
+        }
+    }
+}
