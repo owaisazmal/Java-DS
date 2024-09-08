27357b3fbefc3db2153c8a2ae69537b3521b72e5
owaisazmal <owaiskhan461@gmail.com>
2024-09-07 17:24:24 -0700

    test

--- DS36Runtime.java
+++ DS36Runtime.java
@@ -1,10 +1,35 @@
+import java.util.Stack;
+
 public class DS36Runtime {
+    //test from cursor
     public static void main(String[] args) throws InterruptedException{
         long start = System.nanoTime();
 
         // ---------------Sample program ----------------------
 
-        Thread.sleep(3000);
+       Stack <String> stack = new Stack <String>();
+
+        System.out.println(stack.empty());
+        
+        stack.push("apple");
+        stack.push("samsung");
+        stack.push("volvo");
+        stack.push("honda");
+        stack.push("Nvidia");
+        
+        System.out.println(stack.empty());
+        System.out.println(stack);
+        
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        
+        System.out.println(stack);
+
+        String myFavThing = stack.peek();
+        System.out.println(myFavThing);
+
+        System.out.println(stack.search("apple"));
 
         // ---------------Sample program ----------------------
 
