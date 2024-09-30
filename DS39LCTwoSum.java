1741f9e9dc6b1db7783e8a8e2537b9e994d157d8
owaisazmal <owaiskhan461@gmail.com>
2024-09-30 00:02:37 -0700

    Two sum main fixed

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1,14 +1,19 @@
 
 public class DS39LCTwoSum {
     public static void main(String[] args) {
-        int nums[] = {3,6,7,8};
-        twoSum(nums, 7);
-    } 
+        int nums[] = {3, 6, 7, 8, 4};
+        int[] result = twoSum(nums, 7);  // Store the result
+        if(result.length > 0) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);  // Print the result
+        } else {
+            System.out.println("No solution found");
+        }
+    }
     public static  int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i]+nums[j]==target){
                     return new int[]{i, j};
                 }
             }
