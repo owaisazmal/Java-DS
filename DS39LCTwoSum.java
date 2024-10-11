cfdfce8a0ca9ccdd85ec2a9e03943979f5abe0f5
owaisazmal <owaiskhan461@gmail.com>
2024-10-10 22:46:44 -0700

    169, 242 LC

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -214,4 +214,26 @@ public class DS39LCTwoSum {
 
         return result;
     }
+    //169. Majority Element
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+    }
+    //242. Valid Anagram
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Integer> tCount = new HashMap<>();
+
+        for(int i = 0; i<s.length(); i++){
+            sCount.put(s.charAt(i), 1 + sCount.getOrDefault(s.charAt(i), 0));
+            tCount.put(t.charAt(i), 1 + tCount.getOrDefault(t.charAt(i), 0));
+        }
+
+        return sCount.equals(tCount);
+    }
 }
