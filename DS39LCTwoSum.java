88ecca95006e6108a7762773357542811a80ad77
owaisazmal <owaiskhan461@gmail.com>
2024-10-11 23:47:29 -0700

    LC 268

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -236,4 +236,11 @@ public class DS39LCTwoSum {
 
         return sCount.equals(tCount);
     }
+    //  268 Missing number
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int Tsum = (n * (n + 1)) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+        return Tsum - actualSum;
+    }
 }
