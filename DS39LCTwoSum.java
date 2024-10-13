08d5d8cda92360b7033ab3ae22d2d9bcada022e6
owaisazmal <owaiskhan461@gmail.com>
2024-10-12 17:17:40 -0700

    LC 11

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -243,4 +243,22 @@ public class DS39LCTwoSum {
         int actualSum = Arrays.stream(nums).sum();
         return Tsum - actualSum;
     }
+    //11. Container With Most Water -med
+    public int maxArea(int[] height) {
+        int max_area = 0;
+        int a_pointer = 0;
+        int b_pointer = height.length - 1;
+        while (a_pointer < b_pointer){
+            if(height[a_pointer] < height[b_pointer]){
+                max_area = Math.max(max_area, height[a_pointer]*(b_pointer-a_pointer));
+                a_pointer  += 1;
+
+            }
+            else{
+                max_area = Math.max(max_area, height[b_pointer] * (b_pointer-a_pointer));
+                b_pointer -= 1;
+            }
+        }
+        return max_area;
+    }
 }
