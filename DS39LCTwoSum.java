4d2d8af065cdecd3bdd7a1bf5ae53c2a54adfd38
owaisazmal <owaiskhan461@gmail.com>
2024-10-18 10:57:55 -0700

    LC 80

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -321,4 +321,19 @@ public class DS39LCTwoSum {
         
         return true;
     }
+    //80. Remove Duplicates from Sorted Array II
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;  // No need to process if the array has 2 or fewer elements
+        }
+
+        int i = 2;
+        for(int j = 2; j<nums.length; j++){
+            if(nums[j] != nums[i-2]){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
 }
