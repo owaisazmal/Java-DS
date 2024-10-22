20dcc742457862b650c30e8e95f9da29690400da
owaisazmal <owaiskhan461@gmail.com>
2024-10-21 19:58:43 -0700

    LC 121

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -336,4 +336,19 @@ public class DS39LCTwoSum {
         }
         return i;
     }
+    //121. Best Time to Buy and Sell Stock
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE; 
+        int max = 0;
+
+        for(int i = 0; i<prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            } else if(prices[i] - min > max){
+                max = prices[i] - min;
+            }
+        } 
+        return max;
+
+    }
 }
