a62c26e20ac75705b12242f29165dcc3e02dc9db
owaisazmal <owaiskhan461@gmail.com>
2024-10-23 17:05:42 -0700

    LC 392

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -372,4 +372,18 @@ public class DS39LCTwoSum {
         // Return -1 if the needle is not found
         return -1;
     }
+    //392. Is Subsequence
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+
+        while (sp < s.length() && tp < t.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+
+        return sp == s.length();        
+    }
 }
