ba85001023458c1d00ff618b7e0ceb5890c8c69c
owaisazmal <owaiskhan461@gmail.com>
2024-10-29 22:12:41 -0700

    twoSum with HashMap

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -494,4 +494,20 @@ public class DS39LCTwoSum {
 
         return true; 
     }
+    //Two sum with Hash Map
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<nums.length; i++){
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
 }
