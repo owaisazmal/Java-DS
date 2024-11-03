72f6136cf0b1d9d897c773976a806f1eea2531bc
owaisazmal <owaiskhan461@gmail.com>
2024-11-02 21:00:23 -0700

    LC228

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -528,4 +528,22 @@ public class DS39LCTwoSum {
         }
         return sum == 1;
     }
+    //228. Summary Ranges
+    public List<String> summaryRanges(int[] nums) {
+        LinkedList<String> result = new LinkedList<>();
+        int n = nums.length;
+
+        if(n == 0) return result;
+
+        int a = nums[0];
+        for(int i = 0; i<n; i++){
+            if(i == n-1 || nums[i]+1 != nums[i+1]){
+                if(nums[i] != a) result.add(a + "->" + nums[i]);
+                else result.add(a + "");
+                if(i != n-1) a=nums[i+1];
+            }
+        }
+        return result;
+
+    }
 }
