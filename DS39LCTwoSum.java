b7dcf1c06bfcde9fa756f92c27a873d206e8a733
owaisazmal <owaiskhan461@gmail.com>
2024-11-03 20:14:52 -0800

    LC 51

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -566,4 +566,20 @@ public class DS39LCTwoSum {
         }
         return ans;
     }
+    //50. Pow(x, n)
+    private double binaryExp(double x, long n){
+        if(n == 0){
+            return 1.0;
+        }
+        if(n < 0){
+            return 1.0 / binaryExp(x, -n);
+        }
+
+        if(n%2 == 1){
+            return x * binaryExp(x * x, (n-1) / 2);
+    
+        }else{
+            return binaryExp(x * x, n / 2);
+        }
+    }
 }
