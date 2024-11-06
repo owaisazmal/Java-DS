d57349747f836e151233936f3704e0b88825467a
owaisazmal <owaiskhan461@gmail.com>
2024-11-05 19:30:34 -0800

    LC 20 Updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -597,4 +597,27 @@ public class DS39LCTwoSum {
 
         return true;
     }
+    //20. Valid Parentheses
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
