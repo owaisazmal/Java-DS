cbc298e3c128150aada1f44093c0426654768095
owaisazmal <owaiskhan461@gmail.com>
2024-11-06 21:12:16 -0800

    HW4 updated

--- Comp482HW4.java
+++ Comp482HW4.java
@@ -75,6 +75,46 @@ public class Comp482HW4 {
             floydWarshall(graph);
         }
     }
+
+    public class OptimalBST {
+        public static double optimalBST(double[] p, double[] q, int n) {
+            double[][] e = new double[n + 2][n + 1];
+            double[][] w = new double[n + 2][n + 1];
+            int[][] root = new int[n + 1][n + 1];
+    
+            for (int i = 1; i <= n + 1; i++) {
+                e[i][i - 1] = q[i - 1];
+                w[i][i - 1] = q[i - 1];
+            }
+    
+            for (int l = 1; l <= n; l++) {
+                for (int i = 1; i <= n - l + 1; i++) {
+                    int j = i + l - 1;
+                    e[i][j] = Double.MAX_VALUE;
+                    w[i][j] = w[i][j - 1] + p[j] + q[j];
+    
+                    for (int r = i; r <= j; r++) {
+                        double t = e[i][r - 1] + e[r + 1][j] + w[i][j];
+                        if (t < e[i][j]) {
+                            e[i][j] = t;
+                            root[i][j] = r;
+                        }
+                    }
+                }
+            }
+    
+            System.out.println("Optimal Cost: " + e[1][n]);
+            return e[1][n];
+        }
+    
+        public static void main(String[] args) {
+            double[] p = {0, 0.15, 0.10, 0.05, 0.10, 0.20}; // Insert a 0 at index 0
+            double[] q = {0.05, 0.10, 0.05, 0.05, 0.05, 0.10}; // Insert a 0 at index 0
+            int n = p.length - 1;
+            optimalBST(p, q, n);
+        }
+    }
+    
     
     
 }
