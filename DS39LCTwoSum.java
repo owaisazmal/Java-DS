7987e9aae7da76d0c435ff4a5095769370dffead
owaisazmal <owaiskhan461@gmail.com>
2024-11-10 15:35:58 -0800

    LC 55 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -666,4 +666,19 @@ public class DS39LCTwoSum {
         }
         return profit;
     }
+    //55. Jump Game
+    public boolean canJump(int[] nums) {
+        int furthest = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(i>furthest){
+                return false;
+            }
+            furthest = Math.max(furthest, i + nums[i]);
+
+            if(furthest >= nums.length - 1){
+                return true;
+            }
+        }
+        return false;
+    }
 }
