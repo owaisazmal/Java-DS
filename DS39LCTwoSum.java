d758b7fe878c6be23e4a7a0c93f37aa7bdaf62aa
owaisazmal <owaiskhan461@gmail.com>
2024-11-15 10:24:59 -0800

    LC 238 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -753,4 +753,29 @@ public class DS39LCTwoSum {
 
         return ans;
     }
+    //238. Product of Array Except Self
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int[] left_product = new int[n];
+        int[] right_product = new int[n];
+        int[] output_arr = new int[n];
+
+        left_product[0] = 1;
+        right_product[n-1] = 1;
+
+        for(int i = 1; i<n; i++){
+            left_product[i] = nums[i-1] * left_product[i-1];
+        }
+
+        for(int i = n-2; i>=0; i--){
+            right_product[i] = nums[i+1] * right_product[i+1];
+        }
+
+        for(int i= 0; i<n; i++){
+            output_arr[i] = left_product[i] * right_product[i];
+        }
+
+        return output_arr;
+    }
 }
