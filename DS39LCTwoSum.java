8b531a0a0aa512eb6ee1892c868ada06a60b2844
owaisazmal <owaiskhan461@gmail.com>
2024-11-16 11:27:01 -0800

    LC 141

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -778,4 +778,18 @@ public class DS39LCTwoSum {
 
         return output_arr;
     }
+    //141. Linked List Cycle
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
 }
