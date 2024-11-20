55eaef9a3313c6cc3a122d410d2d33fc161471ae
owaisazmal <owaiskhan461@gmail.com>
2024-11-19 23:57:33 -0800

    LC 134

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -869,4 +869,24 @@ public class DS39LCTwoSum {
         Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
+    //134. Gas Station
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0;
+
+        for(int i = 0; i<gas.length; i++){
+            sum += gas[i] - cost[i];
+        }
+        if(sum < 0) return -1;
+
+        int tank = 0;
+        int station = 0;
+        for(int i = 0; i<gas.length;i++){
+            tank += gas[i] - cost[i];
+            if(tank < 0){
+                station = i + 1;
+                tank = 0;
+            }
+        }
+        return station;
+    }
 }
