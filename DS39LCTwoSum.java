2f9e55b053787f37a47de4c025207a5160918139
owaisazmal <owaiskhan461@gmail.com>
2024-12-02 23:18:39 -0800

    LC 2109

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1023,4 +1023,16 @@ public class DS39LCTwoSum {
         }
         return false;
     }
+    //2109. Adding Spaces to a String
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sp = new StringBuilder();
+        sp.append(s);
+        for(int i=0; i<spaces.length;i++){
+            spaces[i]=spaces[i]+i;
+        }
+        for (int c : spaces) {
+            sp.insert(c," ");
+        }
+        return sp.toString();
+    }
 }
