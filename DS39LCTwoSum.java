87480a967807eb3eeee15bf99f4aa84f92a37b85
owaisazmal <owaiskhan461@gmail.com>
2024-12-08 11:09:26 -0800

    LC 135 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1093,5 +1093,22 @@ public class DS39LCTwoSum {
             res = Math.max(res, cnt);
         }
         return res;
-    }   
+    }
+    //135. Candy
+    public int candy(int[] ratings) {
+        int[] ans = new int[ratings.length];
+        Arrays.fill(ans, 1);
+        for(int i =1; i<ratings.length; i++){
+            if(ratings[i] > ratings[i-1]){
+                ans[i] = ans[i-1]+1;
+
+            }
+        }
+        for(int i = ratings.length-1;i>0; i--){
+            if(ratings[i] < ratings[i-1] && ans[i] <= ans[i-1]){
+                ans[i-1] = ans[i] + 1;
+            }
+        }
+        return Arrays.stream(ans).sum();
+    } 
 }
