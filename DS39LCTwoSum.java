8c067c28824472230139892c7303910aa93cc9bc
owaisazmal <owaiskhan461@gmail.com>
2024-12-10 22:23:38 -0800

    LC 6 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1139,5 +1139,26 @@ public class DS39LCTwoSum {
         
         return ans;
     }
+    //6. Zigzag Conversion
+    public String convert(String s, int numRows) {
+        String[] ans = new String[numRows];
+        for(int i = 0; i<numRows; i++){
+            ans[i] = "";
+        }
+        int i =0;
+        while(i<s.length()){
+            for(int index=0; index<numRows && i<s.length(); index++){
+                ans[index]+=s.charAt(i++);
+            }
+            for(int index=numRows - 2; index>0 && i<s.length(); index--){
+                ans[index]+=s.charAt(i++);
+            }
+        }
+        String res ="";
+        for(String str: ans){
+            res+=str;
+        }
+        return res;
+    }
 
 }
