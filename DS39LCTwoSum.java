acd74f5958f1343151f684c7a2c29dfde2400322
owaisazmal <owaiskhan461@gmail.com>
2025-01-30 17:47:19 -0800

    LC 70 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1197,6 +1197,17 @@ public class DS39LCTwoSum {
         }
         return res;
     }
+    //70. Climbing Stairs
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int a =1, b = 2;
+        for(int i = 3; i<=n; i++){
+            int temp = a+b;
+            a=b;
+            b=temp;
+        }
+        return b;
+    }
 
 }
 }
