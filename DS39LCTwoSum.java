f4b51838042248a393a112a2eb708b2837079ccf
owaisazmal <owaiskhan461@gmail.com>
2025-02-01 18:30:54 -0800

    LC 1752 updated

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1208,6 +1208,19 @@ public class DS39LCTwoSum {
         }
         return b;
     }
+    //1752. Check if Array Is Sorted and Rotated
+    public boolean check(int[] nums) {
+        int count=0;
+        for(int i=1; i<nums.length; ++i) {
+            if(nums[i-1] > nums[i]) {
+                count++;
+            }
+        }
+        if(nums[0] < nums[nums.length-1]) {
+            count++;
+        }
+        return count <= 1;
+    }
 
 }
 }
