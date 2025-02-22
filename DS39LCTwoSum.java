7dc53d749238e1a880395a2e945330f8bc8afc14
owaisazmal <owaiskhan461@gmail.com>
2025-02-21 23:49:50 -0800

    Remove unused backtrack method from DS39LCTwoSum.java

--- DS39LCTwoSum.java
+++ DS39LCTwoSum.java
@@ -1339,44 +1339,6 @@ public class DS39LCTwoSum {
         backtrack(0, result, used, n);
         return result;
     }
-    //LC weekly
-    private boolean backtrack(int pos, int[] result, boolean[] used, int n) {
-        if (pos == result.length) {
-            return true;
-        }
-        
-        if (result[pos] != 0) {
-            return backtrack(pos + 1, result, used, n);
-        }
-        
-        for (int i = n; i >= 1; i--) {
-            if (used[i]) continue;
-            
-            if (i == 1) {
-                result[pos] = i;
-                used[i] = true;
-                if (backtrack(pos + 1, result, used, n)) {
-                    return true;
-                }
-                result[pos] = 0;
-                used[i] = false;
-            } else {
-                if (pos + i < result.length && result[pos + i] == 0) {
-                    result[pos] = i;
-                    result[pos + i] = i;
-                    used[i] = true;
-                    if (backtrack(pos + 1, result, used, n)) {
-                        return true;
-                    }
-                    result[pos] = 0;
-                    result[pos + i] = 0;
-                    used[i] = false;
-                }
-            }
-        }
-        
-        return false;
-    }
 
 }
 }
